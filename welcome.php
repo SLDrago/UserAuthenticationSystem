86e4baec7007eb99e9df94c831803ad3c5cb113b
SLDrago <dilshanoshada7@gmail.com>
2024-07-12 13:14:25 +0530

    minor change

--- welcome.php
+++ welcome.php
@@ -17,7 +17,7 @@ $lname = $_SESSION['lname'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
